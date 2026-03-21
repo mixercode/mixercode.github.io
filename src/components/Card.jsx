418078f061f18a39d19d49dca6591f40f45f6bbc
Miguel Flores <jmiguel.flores121@gmail.com>
2026-03-20 21:47:08 -0600

    `Refactor Card component to accept isRounded prop and update CertificatesSection to use it`

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,10 +5,13 @@ export default function Card({
   children,
   className = "",
   allowOverflow = false,
+  isRounded = true,
 }) {
+  const roundedClasses = isRounded ? "rounded-[48px]" : "rounded-[15px]";
+
   return (
     <ParticleCard
-      className={`spotlight-card backdrop-blur-md bg-[#151926cc] border border-[#2A2F3E] rounded-[48px] drop-shadow-2xl ${className}`}
+      className={`spotlight-card backdrop-blur-md bg-[#151926cc] border border-[#2A2F3E] ${roundedClasses} drop-shadow-2xl ${className}`}
       enableTilt={true}
       clickEffect={true}
       glowColor="22, 74, 232"
--- src/components/CertificatesSection.jsx
+++ src/components/CertificatesSection.jsx
@@ -11,11 +11,11 @@ export default function CertificatesSection() {
 
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 w-full">
         {CERTIFICATES_DATA.map((cert) => (
-          <Card key={cert.id} className="flex flex-col h-full overflow-hidden">
+          <Card key={cert.id} isRounded={false}>
             <div
-              className="w-full h-48 overflow-hidden relative group"
+              className="w-full h-35 overflow-hidden relative group"
               onClick={() => openModal(cert)}
             >
               <img
@@ -28,12 +28,10 @@ export default function CertificatesSection() {
               <div className="absolute inset-0 bg-[#303a58b0] group-hover:bg-transparent transition-colors duration-300" />
             </div>
 
-            <div className="p-6 grow flex flex-col justify-between">
-              <div>
-                <h3 className="text-xl font-bold text-gray-100 mb-2 leading-tight">
-                  {cert.title}
-                </h3>
-              </div>
+            <div className="p-3 grow flex flex-col items-center text-center ">
+              <h3 className="text-lg  text-gray-100 mb-2 leading-tight">
+                {cert.title}
+              </h3>
             </div>
           </Card>
         ))}
